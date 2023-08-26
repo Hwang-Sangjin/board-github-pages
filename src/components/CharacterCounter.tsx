5e705619d1a691207dce5d69c0def837a16cd0dc
Sangjin Hwang <aosdb5640@naver.com>
2023-08-26 16:27:33 +0900

    add: login page

---
+++ src/components/CharacterCounter.tsx
@@ -0,0 +1,38 @@
+import { useRecoilState,useRecoilValue } from "recoil";
+import charCountState from "../states/charCountState";
+import textState from "../states/state";
+
+function CharacterCounter() {
+    return (
+      <div>
+        <TextInput />
+        <CharacterCount />
+      </div>
+    );
+  }
+
+  
+
+  function CharacterCount() {
+    const count = useRecoilValue(charCountState);
+  
+    return <>Character Count: {count}</>;
+  }
+  
+  function TextInput() {
+    const [text, setText] = useRecoilState(textState);
+  
+    const onChange = (event) => {
+      setText(event.target.value);
+    };
+  
+    return (
+      <div>
+        <input type="text" value={text} onChange={onChange} />
+        <br />
+        Echo: {text}
+      </div>
+    );
+  }
+
+  export default CharacterCounter
---
+++ src/states/charCountState.ts
@@ -0,0 +1,13 @@
+import { atom,selector } from "recoil";
+import textState from "./state";
+
+const charCountState = selector({
+    key: 'charCountState', // unique ID (with respect to other atoms/selectors)
+    get: ({get}) => {
+      const text = get(textState);
+  
+      return text.length;
+    },
+  });
+
+  export default charCountState
